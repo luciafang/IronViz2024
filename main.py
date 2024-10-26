0e64c372741cffc22ce4f18eea320414bd8334a7
luciafang <yufang@andrew.cmu.edu>
2024-10-26 18:53:16 -0400

    no fig3

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ fig1, options1 = fig1(left)
 fig2, options2 = fig2(right)
 left.plotly_chart(fig1, use_container_width=True)
 right.plotly_chart(fig2, use_container_width=True)
-left.plotly_chart(fig3(left, options1), use_container_width=True)
+# left.plotly_chart(fig3(left, options1), use_container_width=True)
 # right.plotly_chart(fig4(right, options2), use_container_width=True)
 
